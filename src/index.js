1626daa43581c815414950936757dcceade3fb67
atikahe <atikah.efnd@gmail.com>
2019-10-21 10:55:48 +0700

    Serve todo list app:

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require('express'); //calling the routing framework
 const api = express(); //initializing the routing tool
 
+api.use(express.static(__dirname + '/public')); //to access the index.html in public folder
+
 api.listen(3000, () => {
   console.log('API up and running!');
 });
 
-api.get('/', (req,res) => {
-  console.log(req);
-  res.send('Hello wurld');
-});
+// api.get('/', (req,res) => {
+//   console.log(req);
+//   res.send('Hello wurld');
+// });
+
+// api.use((req,res,next) => {
+//   console.log('Hello');
+//   next();
+// })
